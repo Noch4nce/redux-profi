4a2382869c6e6e0a5977ca6c672557df18c352ec
Noch4nce <m4rgera123@mail.ru>
2022-09-17 21:59:06 +0500

    users dispatch

--- src/store/reducers/ActionCeators.ts
+++ src/store/reducers/ActionCeators.ts
@@ -1,11 +1,24 @@
 import { AppDispatch } from '../store'
 import axios from 'axios'
 import { IUser } from '../../models/IUser'
+import { userSlice } from './UserSlice'
 
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
 	try {
+		dispatch(userSlice.actions.usersFetching())
 		const response = await axios.get<IUser[]>(
 			'https://jsonplaceholder.typicode.com/users'
 		)
-	} catch (error) {}
+		console.log(response, 'response')
+		const data = response.data
+		console.log(data, 'data')
+		dispatch(userSlice.actions.userFetchingSuccess(data))
+	} catch (error) {
+		let errorMessage = 'Response Error'
+		if (error instanceof Error) {
+			errorMessage = error.message
+		}
+
+		dispatch(userSlice.actions.userFetchingError(errorMessage))
+	}
 }
