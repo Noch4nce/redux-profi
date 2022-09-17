09579bd0cc27b4359ba59dbcde918467525d377a
Noch4nce <m4rgera123@mail.ru>
2022-09-17 19:04:43 +0500

    user slice

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,4 +1,5 @@
 import { IUser } from '../../models/IUser'
+import { createSlice } from '@reduxjs/toolkit'
 
 interface UserState {
 	users: IUser[]
@@ -6,6 +7,18 @@ interface UserState {
 	error: string
 }
 
-const initialState = {
-
+const initialState: UserState = {
+	users: [],
+	isLoading: false,
+	error: ''
 }
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {
+
+	}
+})
+
+export default userSlice.reducer
