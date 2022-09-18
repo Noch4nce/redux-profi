9a763520a06d65fabeffdb8799f33025c33231b2
Noch4nce <m4rgera123@mail.ru>
2022-09-18 21:57:27 +0500

    post loading error

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -3,11 +3,13 @@ import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
-	const { data: posts } = postAPI.useFetchAllPostsQuery(5)
+	const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(5)
 
 	return (
 		<div>
 			<div className="post__list">
+				{isLoading && <h1>Loading...</h1>}
+				{error && <h1>Response error</h1>}
 				{posts &&
 					posts.map((post) => <PostItem key={post.id} post={post} />)}
 			</div>
